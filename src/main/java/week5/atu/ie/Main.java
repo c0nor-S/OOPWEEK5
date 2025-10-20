4e16330088420d8201b2eaa15d31b1f86f419064
CONORSHAUGHNESSY-STU <g00432599@atu.ie>
2025-10-20 10:24:02 +0100

    Second Commit - Used Try-With-Resources To Create & Write To A File

--- src/main/java/week5/atu/ie/Main.java
+++ src/main/java/week5/atu/ie/Main.java
@@ -1,24 +1,28 @@
 package week5.atu.ie;
 
+import java.io.*;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-            int age = 0;
+        System.out.println("Enter The File Name (e.g. students.txt): ");
+        String fileName = sc.nextLine().trim();
 
-            try {
-                System.out.println("Enter Your Age: ");
-                String text = sc.nextLine().trim();
+        System.out.println("Enter A Name To Save: ");
+        String name = sc.nextLine().trim();
 
-                age = Integer.parseInt(text);
-                System.out.println("Thanks! You Entered: " + age);
-            }
-            catch (NumberFormatException e) {
-                System.out.println("Invalid Input: " + e.getMessage());
-            }
+        try(PrintWriter out = new PrintWriter(new FileWriter(fileName, true)))
+        {
+            out.println(name);
+            System.out.println("Saved To: " + fileName);
+        }
+
+        catch (IOException ex) {
+            System.out.println("Could Not Write To File: " + ex.getMessage());
+        }
     }
 }
